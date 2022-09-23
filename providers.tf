a85f39147a21bab49ea949870a4b0ec41253d2b8
dbbenatti <doonerbpt@gmail.com>
2022-09-23 12:35:33 -0300

    delete gcpkey

--- providers.tf
+++ providers.tf
@@ -10,8 +10,6 @@ terraform {
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  credentials = file("/home/danyllobenatti/gcpkey.json")
-
   project = "quantum-star-360500"
   region  = "us-west1"
   zone    = "us-west1-b"
